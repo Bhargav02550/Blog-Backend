532a12ccb63c1c734bce8bbf52307f03d63dc445
bhargav0255 <bhargav.pavan.025@gmail.com>
2024-08-18 11:46:10 +0530

    Added register and login

--- controllers/Postcontrollers.js
+++ controllers/Postcontrollers.js
@@ -1,6 +1,6 @@
 const { get } = require("mongoose");
 const Post = require("../models/post");
-
+const bcrypt = require("bcrypt");
 
 const uploadpost = async (req, res) => {
   try {
@@ -13,19 +13,38 @@ const uploadpost = async (req, res) => {
   }
 };
 
-const getpost = async (req,res) => {
-    try{
-        const post = await Post.find();
-        res.status(200).json(post);
+const getpost = async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: "Error getting post" });
+  }
+};
 
-    }
-    catch(error){
-        res.status(500).json({message:"Error getting post"});
-    }
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const salt = await bcrypt.genSalt(10);
+  const hashedpassword = await bcrypt.hash(password, salt);
+  try {
+    const user = new User({
+      email: email,
+      password: hashedpassword,
+    });
+    const saveduser = await user.save();
+    res.status(201).json(saveduser);
+  } catch (error) {
+    res.status(500).json({ message: "User already registered" });
+  }
+};
+
+const login = async (req,res) => {
+  const {email , password} = req.body;
+  
 }
-
 
 module.exports = {
   uploadpost,
   getpost,
+  register,
 };
--- routes/Postroutes.js
+++ routes/Postroutes.js
@@ -8,5 +8,6 @@ const Postcontroller = require("../controllers/Postcontrollers");
 Router.post("/upload_post",Postcontroller.uploadpost);
 // Router.post("/post_fake_data",Postcontroller.fakedata);
 Router.get("/get_posts",Postcontroller.getpost);
+Router.post("/register",Postcontroller.register);
 
 module.exports = Router;
